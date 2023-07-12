6cdb2a262bdc5ebbe9ddd56cdeac76bf81b290ac
Anushka Bahuguna <anushkabahugunaa@gmail.com>
2023-07-12 19:58:54 +0530

    Add contact detail styles

--- client/src/styles/ContactStyles.js
+++ client/src/styles/ContactStyles.js
@@ -1,6 +1,11 @@
 import { styled } from "@mui/material/styles";
 const Root = styled("div")(({ theme }) => ({
   backgroundColor: "#18191a",
+  letterSpacing: "2px",
+  fontFamily: "Poppins",
+
+  backgroundImage:
+    "radial-gradient( circle 1600px at 40.4% -51.8%,  rgba(9,29,85,1) 0.2%, rgba(0,0,0,1) 100.2% );",
   color: "white",
   height: "100%",
   overflow: "auto",
@@ -11,18 +16,18 @@ const Root = styled("div")(({ theme }) => ({
     padding: "0",
   },
   "& .Contact-heading": {
-    fontSize: "3rem",
+    fontSize: "3.6rem",
     textAlign: "center",
     width: "50%",
     margin: "0 auto",
-    paddingTop: "2rem",
+    paddingTop: "1.5rem",
     fontWeight: "600",
   },
   "& .Contact-body": {
     display: "flex",
     width: "70%",
     margin: "0 auto",
-    marginTop: "3rem",
+    marginTop: "2.5rem",
     gap: "1.5rem",
     // backgroundColor: "grey",
     "& >div": {
@@ -33,24 +38,34 @@ const Root = styled("div")(({ theme }) => ({
     // backgroundColor: "aqua",
   },
   "& .Contact-subdetails": {
-    margin: "0 0 3rem 0",
+    margin: "0 0 2.5rem 0",
     display: "flex",
     gap: "1.2rem",
     fontSize: "1.3rem",
   },
   "& .Contact-subdetails>div:nth-of-type(2)": {
     // backgroundColor: "red",
-    fontSize: "1rem",
+    fontSize: "0.85rem",
     display: "flex",
     gap: "0.5rem",
     flexDirection: "column",
-    fontWeight: "200",
+    fontWeight: "300",
+    letterSpacing: "1px",
     "& span:nth-of-type(1)": {
       fontWeight: "500",
+      fontSize: "1.1rem",
+      letterSpacing: "1.5px",
     },
   },
   "& svg": {
-    fontSize: "1.7rem",
+    fontSize: "1.8rem",
+  },
+  "& a": {
+    color: "#ffffffbf",
+    textDecoration: "none",
+    "&:hover": {
+      borderBottom: "1px solid white",
+    },
   },
 }));
 
